91299b9ab3c09d9236606cd66b1cead8d91a8cb7
Dan Pastusek <dan@hexial.com>
2018-11-25 21:52:49 -0800

    sample events, basic timeline outline

--- src/App.js
+++ src/App.js
@@ -1,28 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import events from './events'
+
+import './App.css'
+
+const categories = ['all', 'education', 'departures', 'foreign', 'domestic', 'trade']
+const originDate = 2016
 
 class App extends Component {
+  state = {
+    category: 'all',
+    zoom: 0
+  }
+
   render() {
+    const { category } = this.state
+    const filteredEvents = events.filter(event => event.category === category)
+
+    const markers = [{ year: 2016 }, { year: 2017 }, { year: 2018 }]
+    const max = 2018
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {categories.map(catIndex => (
+            <div className={catIndex === category ? 'active' : ''}>{catIndex}</div>
+          ))}
         </header>
+        <section className="App-section">
+          {markers.map(({ year }) => (
+            <div
+              className="date"
+              style={{
+                top: `calc(${((year - originDate) / (max - originDate)) * 100 + '%'} - 1vh)`
+              }}
+            >
+              {year}
+            </div>
+          ))}
+          {filteredEvents.map(({ title, description, weight, icon, time }) => {
+            return (
+              <div
+                style={{
+                  top: '10px',
+                  height: weight,
+                  width: weight
+                }}
+              >
+                <img src={icon} alt="icon" />
+                <h2>
+                  {title}
+                  <span className="time">{time}</span>
+                </h2>
+                <p>{description}</p>
+              </div>
+            )
+          })}
+        </section>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/events.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg'
+
+let events = [
+  {
+    category: 'trade',
+    date: '2017-01-01T23:28:56.782Z',
+    title: 'Trump imposes sanctions with China',
+    description: 'Sanctions 1',
+    icon: logo,
+    weight: 10
+  },
+  {
+    category: 'trade',
+    date: '2017-04-01T23:28:56.782Z',
+    title: 'Trump imposes sanctions with China',
+    description: 'Sanctions 2',
+    icon: logo,
+    weight: 10
+  },
+  {
+    category: 'foreign',
+    date: '2018-10-2T10:14Z',
+    title: 'Jamal Khashoggi Killed',
+    description: 'Killed in Turkish Consulate',
+    icon: logo,
+    weight: 10
+  }
+]
+
+export default events
